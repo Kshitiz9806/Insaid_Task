c82279ebc1ba507d2f02237c6f19edfe9c1c1e8c
Kshitiz9806 <subodhkshitiz1971@gmail.com>
2022-12-09 19:40:06 +0530

    frontend start

---
+++ Insaid_frontend/insaid_frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MainPage from './components/MainPage';
+
+class App {
+	render() {
+		return (
+			<div className="App">
+				<MainPage />
+			</div>
+		);
+  	}
+}
+
+export default App;
---
+++ Insaid_frontend/insaid_frontend/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <div className='Ribbon'>
+                Data Science or Product Management: Which career is right for you?
+                <button classNam='btn btn-success'>Talk to an Expert</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
