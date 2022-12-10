06652408567230e9e7512a819bcec4a6de2be19f
Kshitiz9806 <subodhkshitiz1971@gmail.com>
2022-12-10 08:46:24 +0530

    left panel completed

--- Insaid_frontend/insaid_frontend/src/components/LeftPanel.js
+++ Insaid_frontend/insaid_frontend/src/components/LeftPanel.js
@@ -10,7 +10,15 @@ const LeftPanel = () => {
             <br />
             <div className="Text3">Learn with India's leading online school for working professionals</div>
             <br />
+            <br />
             <button className="ChatButton">Talk to an Expert</button>
+            <br />
+            <br />
+            <br />
+            <div className="BlinkingText">
+                <img src="https://www.insaid.co/wp-content/uploads/2020/10/h2teacher-50x50.png" alt="studying logo" className="Logo1"/>
+                <div className="Text4">360 degree career support</div>
+            </div>
         </div>
     )
 }
